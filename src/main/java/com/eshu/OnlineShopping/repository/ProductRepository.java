40a1c3cf8cda7b77c994485df83ccff76c29a6d7
Keshav Maheshwari <90709089+kehav1534@users.noreply.github.com>
2026-09-02 22:37:59 +0530

    Update ProductRepository.java

--- src/main/java/com/eshu/OnlineShopping/repository/ProductRepository.java
+++ src/main/java/com/eshu/OnlineShopping/repository/ProductRepository.java
@@ -20,9 +20,11 @@ public interface ProductRepository extends JpaRepository<Product, Integer> {
     @Query(nativeQuery = true, value = "SELECT * from products WHERE category IN :categories")
     public Page<Product> productWithCategory(@Param("categories") List<String> category, Pageable pageable);
 
-    @Query(nativeQuery = false, value = "SELECT p FROM Product p WHERE (:minPrice is NULL OR p.price >= :minPrice) AND (:maxPrice is NULL OR p.price <= :maxPrice) and (:productName is NULL or LOWER(p.name) LIKE LOWER(CONCAT('%',:productName,'%'))) AND (:categories is NULL or p.category in :categories) AND (:manufacturer is NULL or p.manufacturer in :manufacturer) AND(p.listingStatus is NULL or p.listingStatus = LIVE or p.availabilityStatus = AVAILABLE)")
-    public List<Product> searchProduct(Integer minPrice, Integer maxPrice, String productName, List<ProductCategory> categories, List<String> manufacturer, Pageable pageable);
-    //for now status works even if it's null.
+    @Query(""" 
+        SELECT p FROM Product p WHERE (:minPrice IS NULL OR p.price >= :minPrice) AND (:maxPrice IS NULL OR p.price <= :maxPrice) AND ( :productName IS NULL OR LOWER(p.name) LIKE CONCAT('%', LOWER(CAST(:productName AS string)), '%') ) AND (:categories IS NULL OR p.category IN :categories) AND (:manufacturer IS NULL OR p.manufacturer IN :manufacturer) AND ( p.listingStatus IS NULL OR p.listingStatus = com.eshu.OnlineShopping.enums.ProductListingStatus.LIVE OR p.availabilityStatus = com.eshu.OnlineShopping.enums.AvailabilityStatus.AVAILABLE ) 
+        """)
+    public List<Product> searchProduct( @Param("minPrice") Integer minPrice, @Param("maxPrice") Integer maxPrice, @Param("productName") String productName, @Param("categories") List<ProductCategory> categories, @Param("manufacturer") List<String> manufacturer, Pageable pageable );
+
 
     Optional<Product> findByIdAndSellerIdId(Integer productId, Integer sellerId);
 
